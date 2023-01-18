153a7db37c920a5b7ba0c028f8e580ecc7c4743a
Agata <118021948+Fermatka@users.noreply.github.com>
2023-01-18 20:39:43 +0100

    New changes in my homework
    Please check if it's correct.

--- script.js
+++ script.js
@@ -3,57 +3,83 @@ console.log("test");
 // Zadanie 1 - 1 punkt
 // Stwórz funkcję 'add', która przyjmuje 2 parametry - 'a' i 'b' oraz zwraca ich sumę. Funkcja powinna sprawdzić, czy oba parametry są typu 'number' (podpowiedź: operator typeof)
 console.log("ZADANIE 1");
-// tego chyba nie musi tu być? :
-// let a = 6;
-// let b = 7;
-
-console.log(typeof a);
-console.log(typeof b);
 
 function add(a, b) {
-  return a + b;
+  if (typeof a == "number" && typeof b == "number") {
+    console.log(`${a} & ${b} is a number`);
+    return a + b;
+  } else {
+    console.log(`${a} or ${b} is not a number- cannon execute the function`);
+    return 0;
+  }
 }
-
-console.log(add(2, 5));
+console.log(`Suma tych dwóch liczb to: ${sum(2, 5)}`);
 
 // Zadanie 2 - 3 punkty
 // Wykonaj funkcje odejmującą, mnożącą oraz dzielącą 2 wartości przekazane w parametrze (3 funkcje).
 
 console.log("ZADANIE 2");
 // SUMA
-let c = 6;
-let d = 2;
-function sum(c, d) {
-  if (typeof c == "number" && typeof d == "number") {
-    console.log(`${c} & ${d} is a number`);
-    return c + d;
+
+function sum(a, b) {
+  if (typeof a == "number" && typeof b == "number") {
+    console.log(`${a} & ${b} is a number`);
+    return a + b;
   } else {
-    console.log("c or d is not a number- cannon execute the function ");
+    console.log("a or b is not a number- cannon execute the function ");
+    return 0;
   }
 }
 console.log(sum(6, 2));
+
 // RÓŻNICA
 function diff(a, b) {
-  //    tu jeszcze muszę dopisać to z typeOf
-  return a - b;
+  if (typeof a == "number" && typeof b == "number") {
+    console.log(`${a} & ${b} is a number`);
+    return a - b;
+  } else {
+    console.log(`${a} or ${b} is not a number- cannon execute the function`);
+    return 0;
+  }
 }
 console.log(diff(5, 2));
 
 // DZIELENIE
 
-function division(e, f) {
-  if (typeof e == "number" && typeof f == "number" && f > 0) {
+function division(a, b) {
+  if (typeof a == "number" && typeof b == "number" && b != 0) {
     console.log(
-      `${e} & ${f} is a number and possible operation - divider is greater than zero`
+      `${a} & ${b} is a number and possible operation - divider is greater than zero`
     );
-    return e / f;
+    return a / b;
   } else {
     console.log(
-      "c or d is not a number or divider = 0 -> cannot execute the function "
+      `${a} or ${b} is not a number or divider = 0 -> cannot execute the function`
     );
   }
+  return 0;
 }
+
 console.log(division(10, 0));
+
+// drugie rozwiązanie- trzy operacje za pomocą jednej funkcji
+console.log("Zadanie2- drugie rozwiązanie");
+function operation(a, b, c) {
+  if (typeof a == "number" && typeof b == "number" && c == "sum") {
+    console.log(`${a} & ${b} is a number`);
+    return a + b;
+  } else if (typeof a == "number" && typeof b == "number" && c == "diff") {
+    console.log(`${a} & ${b} is a number`);
+    return a - b;
+  } else if (typeof a == "number" && typeof b == "number" && c == "div") {
+    console.log(`${a} & ${b} is a number`);
+    return a / b;
+  } else return false;
+}
+
+console.log(`Sum of a and b is: ${operation(2, 3, "sum")}.`);
+console.log(`Difference a and b is: ${operation(5, 1, "diff")}.`);
+console.log(`Division of a and b is:${operation(10, 2, "div")}.`);
 
 // Wynik arytmetyczny powinien wyświetlić się na konsoli Jeżeli któryś z zadanych parametrów nie będzię liczbą, funkcja powinna zwrócić napis: "Niepoprawny parametr". Jeżeli funkcja zostanie uruchomiona tylko z jednym parametrem, powinien zostać on wyświetlony jako wynik działania funkcji.
 
@@ -65,11 +91,30 @@ function theBiggestNumber(a, b, c) {
     console.log(`${a} is the biggest number`);
   } else if (b > a && b > c) {
     console.log(`${b} is the biggest number`);
-  } else console.log(`${c} is the biggest number`);
+  } else {
+    console.log(`${c} is the biggest number`);
+  }
 }
 console.log(theBiggestNumber(9, 2, 3));
+
 // Zadanie 4 - 3 punkty
 // Stwórz funkcje która przyjmie 3 argumenty (age,height, weight). Na podstawie wprowadzonych danych, Oblicz i wyświelt w konsoli BMI dna podanych danych. Nalezy sprawdzić czy nasze wprowadzne dane są liczbami, jeśli jakakolwiek z nich nie jest nalezy wyświetlić komunikat: 'Błędne dane ${dane}'
+
+// BMI obliczamy dzieląc masę ciała (w kilogramach) przez wzrost do kwadratu (w metrach).
+function BMI(age, height, weight) {
+  if (typeof age != "number") {
+    console.log(`Bledne dane (age): ${age}`);
+  } else if (typeof height != "number") {
+    console.log(`Bledne dane (height): ${height}`);
+  } else if (typeof weight != "number") {
+    console.log(`Bledne dane (weight): ${weight}`);
+  } else {
+    return weight / height ** 2;
+  }
+}
+
+console.log(BMI(2, 2, 40));
+// co powinna zwrócić funkcja przy "nagatywnym" rozliczeniu, nie ma return? "błąd"??
 
 // Uwagi
 // Kod piszemy camelCasem i tylko po angielsku
